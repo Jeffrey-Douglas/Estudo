17572099e64492ba09687933d383aaa25bee7dc0
jeffrey_santos <jeffrey.santos@datagrupo.com.br>
2020-12-18 14:26:11 -0300

    Criando minha propria Exception

--- src/CriandoException/UsandoMinhaException.java
+++ src/CriandoException/UsandoMinhaException.java
@@ -1,2 +1,23 @@
-package CriandoException;public class UsandoMinhaException {
+package CriandoException;
+
+public class UsandoMinhaException {
+
+    public static void main(String[] args) {
+        int[] numeros = {4, 8, 5, 16, 32, 21, 64, 128};
+        int[] denom = {2, 0, 4, 8, 0, 2, 3};
+
+        for (int i = 0; i < numeros.length; i++) {
+            try {
+                if (numeros[i] % 2 != 0) {
+                    throw new DivisaoNaoExata(numeros[i], denom[i]);
+                }
+                System.out.println(numeros[i] + " / " + denom[i] + " = " + (numeros[i]/denom[i]));
+
+            } catch (ArrayIndexOutOfBoundsException | ArithmeticException | DivisaoNaoExata e) {
+                System.out.println("Aconetecu um erro ");
+                e.printStackTrace();
+
+            }
+        }
+    }
 }
